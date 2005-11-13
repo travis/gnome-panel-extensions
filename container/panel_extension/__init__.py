cb845c15df2d0219f0c8f6dd446fbf1777c6068f
tvachon <tvachon@04d3b6d0-ec25-0410-b116-fecf52fb07e1>
2005-11-13 06:29:15 +0000

    documentation and rearranging
    git-svn-id: http://svn.gnome.org/svn/gnome-panel-extensions/trunk@50 04d3b6d0-ec25-0410-b116-fecf52fb07e1

--- container/panel_extension/__init__.py
+++ container/panel_extension/__init__.py
@@ -1,10 +1,22 @@
-'''
-panel_extension package
-(c)2005 travis f vachon
+'''Panel_extension package.
+(c)2005 Travis F Vachon
 
-the panel_extension package provides modules needed to run GNOME
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+ You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+02111-1307, USA.
+
+The panel_extension package provides modules needed to run GNOME
 panel extensions. For more information on these, please see
-http://www.gnome.org/~tvachon
+http://www.gnome.org/~tvachon.
 '''
 import pygtk
 pygtk.require('2.0')
@@ -15,13 +27,22 @@ import gtk
 import gobject
 
 class NoAppletError(Exception):
+	'''Raised when extensions are run without a container applet.
+
+	Running extensions in this way make bundle resources and some
+	pieces of the API unavailable.'''
 	def __init__(self, *args):
 		Exception.__init__(self, *args)
 		self.wrapped_exc = sys.exc_info()
 	
-#A super class for all extensions
+
 class PanelExtension(gtk.EventBox):
-	
+	'''A super class for all extensions.
+
+	All panel extensions should subclass this class. PanelExtension
+	provides a number of very useful functions for accessing bundle
+	resources and system utilities like gconf preference keys.
+	'''
 
 	def __init__(self, bundle=None):
 		self.__gobject_init__()
@@ -35,6 +56,12 @@ class PanelExtension(gtk.EventBox):
 
 
 	def get_bundle(self):
+		'''Returns the Bundle containing this extension.
+
+		This function returns a Bundle object representing the
+		bundle containing the extension which provides methods to
+		access bundle resources.
+		'''
 		if self.bundle:
 			return self.bundle
 		elif self.applet:
@@ -42,12 +69,31 @@ class PanelExtension(gtk.EventBox):
 		else:
 			raise NoAppletError, "No applet registered, cannot get bundle"
 
+	##@param key The name of the gconf key to create in /apps/panel/extensions.
  	def get_preferences_key(self, key):
+		'''Returns a gconf preferences key for extensions.
+		
+		The provided key parameter should be unique or risk
+		interaction with other extensions. They can be any
+		combination of alphanumeric characters and underscores.
+		A url should work well, for example:
+		    www_gnome_org_tvachon_sampleextension
+		'''
 		return os.path.normpath("/apps/panel/extensions"+key)
 				
 			
 
+	##
+	#@param xml string containing Bonobo user interface XML
+	#@param verbs Bonobo user interface verbs
 	def setup_extension_menu(self, xml, verbs):
+		'''Set up a context menu from string of XML.
+
+		The XML must be Bonobo user interface XML. More information
+		about this XML can be found at
+		<a href="http://www.pycage.de/howto_bonobo.html">
+		http://www.pycage.de/howto_bonobo.html</a>
+		'''
 		if self.live_preview_mode:
 			pass
 		
@@ -55,8 +101,18 @@ class PanelExtension(gtk.EventBox):
 			self.applet.setup_menu(xml, verbs, None)
 		else:
 			raise NoAppletError, "No applet file registered, can not create context menu"
-		
+
+	##
+	#@file name of file in bundle containing interface xml
 	def setup_extension_menu_from_file(self, file, verbs):
+		'''Set up a context menu from XML contained in file in bundle.
+
+		The file parameter should be a file in the bundle.
+		The XML must be Bonobo user interface XML. More information
+		about this XML can be found at
+		<a href="http://www.pycage.de/howto_bonobo.html">
+		http://www.pycage.de/howto_bonobo.html</a>
+		'''
 		if self.live_preview_mode:
 			pass
 
